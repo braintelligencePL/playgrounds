b0120197a51cbd1fd282f83cdbb46a22ca664303
lsroczynski <lsroczynski@consdata.pl>
2018-11-13 12:30:00 +0100

    project manager - multi-module-project

---
+++ projects/project-management/project-management-backend/src/main/kotlin/pl/braintelligence/projectmanager/Application.kt
@@ -0,0 +1,19 @@
+package pl.braintelligence.projectmanager
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+class Application {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+}
+
