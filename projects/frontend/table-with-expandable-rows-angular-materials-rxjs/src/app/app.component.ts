f6888767aac08587ed72f20b2ae7f7a9089617dc
lsroczynski <lsroczynski@consdata.pl>
2019-01-16 16:41:15 +0100

    ...

---
+++ projects/frontend/table-with-expandable-rows-angular-materials-rxjs/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+<p>XX</p>
+  `,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'table-with-expandable-rows-angular-materials-rxjs';
+}
